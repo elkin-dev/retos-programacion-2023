d32e3e29d50db47be5d77967e8aaad9e676bf4a8
elkin-dev <112489597+elkin-dev@users.noreply.github.com>
2023-12-23 12:35:17 -0500

    Reto #4 PRIMO, FIBONACCI Y PAR

--- Retos/Reto #4 - PRIMO, FIBONACCI Y PAR [Media]/python/codekin.py
+++ Retos/Reto #4 - PRIMO, FIBONACCI Y PAR [Media]/python/codekin.py
@@ -1,3 +1,4 @@
+<<<<<<< HEAD
 """ 
  * Escribe un programa que, dado un número, compruebe y muestre si es primo, fibonacci y par.
  * Ejemplos:
@@ -53,3 +54,60 @@ def checkNumber(n):
 numbers = 7
 if __name__ == "__main__":
     checkNumber(numbers)
+=======
+""" 
+ * Escribe un programa que, dado un número, compruebe y muestre si es primo, fibonacci y par.
+ * Ejemplos:
+ * - Con el número 2, nos dirá: "2 es primo, fibonacci y es par"
+ * - Con el número 7, nos dirá: "7 es primo, no es fibonacci y es impar"
+"""
+from math import sqrt
+
+
+def checkNumber(n):
+    def primo(n):
+        if n > 1 and all(n % i != 0 for i in range(2, int(sqrt(n)) + 1)):
+            return True
+        return False
+
+    def par(n):
+        if n != 0 and n % 2 == 0:
+            return True
+        return False
+
+    def fibonacci(n):
+        a, b = 0, 1
+        for _ in range(n):
+            if a == n:
+                return True
+            a, b = b, a + b
+        return False
+
+    is_prime = primo(n)
+    is_even = par(n)
+    is_fibonacci = fibonacci(n)
+
+    result = f"{n} es "
+
+    if is_prime:
+        result += "primo"
+    else:
+        result += "no es primo"
+
+    if is_fibonacci:
+        result += ", fibonacci"
+    else:
+        result += ", no es fibonacci"
+
+    if is_even:
+        result += " y es par"
+    else:
+        result += " y es impar"
+
+    print(result)
+
+
+numbers = 7
+if __name__ == "__main__":
+    checkNumber(numbers)
+>>>>>>> 8f204d06d (Reto #4 PRIMO, FIBONACCI Y PAR)
